de7a66443ff649d3ad933853889522e14ba1070d
supra321 <suprativdatta@gmail.com>
2020-03-21 19:52:11 +0530

    port changes

--- server.js
+++ server.js
@@ -24,14 +24,14 @@ const app=express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/',(req,res)=>{res.send("Mutton")/*rooot.handleRootGet(res,database)*/})
+app.get('/',(req,res)=>{rooot.handleRootGet(res,database)})
 app.post('/login',(req,res)=>{login.handleLogInPost(req,res,bcrypt,database)})
 app.post('/signup',(req,res)=>{signup.handleSignUpPost(req,res,bcrypt,database)})
 app.get('/profile/:id',(req,res)=>{profile.handleProfileGet(req,res,database)})
 app.put('/rank',(req,res)=>{rank.handleRankPut(req,res,database)})
 app.post('/imageurl',(req,res)=>{rank.handleAPICallPost(req,res)})
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`App is running on port ${ PORT }`);
+const PORT=process.env.PORT||3000;
+app.listen(PORT,()=>{
+    console.log(`App is running on port ${PORT}`);
 });
